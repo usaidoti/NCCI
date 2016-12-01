1ef089173871d5decc76b66f8af43a5543de6fee
snurhussein <safy@modernthings.net>
2016-12-01 16:13:59 -0500

    Update limerscript.R

--- limerscript.R
+++ limerscript.R
@@ -9,8 +9,8 @@ library(reshape2)
 #connect to limer, change api link, username and password where necessary
 
 options(lime_api = 'http://survey.itechcenter.ne/index.php/admin/remotecontrol')
-options(lime_username = 'XXXXX')
-options(lime_password = 'XXXXX')
+options(lime_username = 'your_username')
+options(lime_password = 'your_password')
 get_session_key()
 
 survey_df<-call_limer(method='list_surveys')
@@ -40,3 +40,80 @@ TILL007<-get_responses(iSurveyID= 943461, sLanguageCode = 'fr', sResponseType = 
 TILL009<-get_responses(iSurveyID= 541311, sLanguageCode = 'fr', sResponseType = 'short')
 DIF040<-get_responses(iSurveyID= 448111, sLanguageCode = 'fr', sResponseType = 'short')
 DIF048<-get_responses(iSurveyID= 716422, sLanguageCode = 'fr', sResponseType = 'short')
+
+#Move misaligned columns to desired, consistent location
+
+AGA059c$Q41.1.<-AGA059c$Q23.1.
+AGA059c$Q41.2.<-AGA059c$Q23.2.
+AGA059c$Q41.3.<-AGA059c$Q23.3.
+AGA059c$Q41.4.<-AGA059c$Q23.4.
+
+AGA060c$Q41.1.<-AGA060c$Q23.1.
+AGA060c$Q41.2.<-AGA060c$Q23.2.
+AGA060c$Q41.3.<-AGA060c$Q23.3.
+AGA060c$Q41.4.<-AGA060c$Q23.4.
+
+TILL007c$Q41.1.<-TILL007c$Q23.1.
+TILL007c$Q41.2.<-TILL007c$Q23.2.
+TILL007c$Q41.3.<-TILL007c$Q23.3.
+TILL007c$Q41.4.<-TILL007c$Q23.4.
+
+TILL009c$Q41.1.<-TILL009c$Q25.1.
+TILL009c$Q41.2.<-TILL009c$Q25.2.
+TILL009c$Q41.3.<-TILL009c$Q25.3.
+TILL009c$Q41.4.<-TILL009c$Q25.4.
+
+TILL009c$Q41.1.<-TILL009c$Q25.1.
+TILL009c$Q41.2.<-TILL009c$Q25.2.
+TILL009c$Q41.3.<-TILL009c$Q25.3.
+TILL009c$Q41.4.<-TILL009c$Q25.4.
+
+DIF040c$Q41.1.<-DIF040c$Q16.1.
+DIF040c$Q41.2.<-DIF040c$Q16.2.
+DIF040c$Q41.3.<-DIF040c$Q16.3.
+DIF040c$Q41.4.<- NA
+DIF040c$Q17<-DIF048c$Q15
+
+DIF048c$Q41.1.<-DIF048c$Q16.1.
+DIF048c$Q41.2.<-DIF048c$Q16.2.
+DIF048c$Q41.3.<-DIF048c$Q16.3.
+DIF048c$Q41.4.<- NA
+DIF048c$Q17<-DIF048c$Q15
+
+
+AGA059c$Q23.1.<-NULL
+AGA059c$Q23.2.<-NULL
+AGA059c$Q23.3.<-NULL
+AGA059c$Q23.4.<-NULL
+
+AGA060c$Q23.1.<-NULL
+AGA060c$Q23.2.<-NULL
+AGA060c$Q23.3.<-NULL
+AGA060c$Q23.4.<-NULL
+
+TILL007c$Q23.1.<-NULL
+TILL007c$Q23.2.<-NULL
+TILL007c$Q23.3.<-NULL
+TILL007c$Q23.4.<-NULL
+
+AGA060c$Q17<-AGA060c$Q16
+AGA060c$Q16<-NULL
+
+TILL009c$Q25.1.<-NULL
+TILL009c$Q25.2.<-NULL
+TILL009c$Q25.3.<-NULL
+TILL009c$Q25.4.<-NULL
+
+#DIF040c$Q16.1.<-NULL
+#DIF040c$Q16.2.<-NULL
+#DIF040c$Q16.3.<-NULL
+#DIF040c$Q15<-NULL
+
+DIF048c$Q16.1.<-NULL
+DIF048c$Q16.2.<-NULL
+DIF048c$Q16.3.<-NULL
+DIF048c$Q15<-NULL
+
+#Comine tables
+
+NCCIbind <- rbind.fill(AGA041c, AGA045c, AGA051c, AGA055c, AGA059c, AGA060c, AGA061c, DIF010c, DIF018c, DIF044c,NIA029c, TILL006c, TILL007c, TILL009c, DIF048c)
